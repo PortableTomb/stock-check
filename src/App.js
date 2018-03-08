b6a6f09429f9467078a120c0ce0e10db8234cee7
PortableTomb <tenderyoungpony@gmail.com>
2018-03-07 19:33:42 -0800

    added stocklist, stocklistitem to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
 import SearchBar from './components/search_bar';
+import StockList from './components/StockList';
 import './App.css';
 
 class App extends Component {
@@ -27,25 +29,30 @@ class App extends Component {
     if(e) e.preventDefault();
     this.setState({
       value: '',
-      term: this.state.value,
-      stocks: this.state.stocks
+      term: this.state.value
     });
+
     let term = this.state.value;
     const key = 'F41ON15LGCFM4PR7';
     const url = `https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=${term}&apikey=${key}`;
 
     axios.get(axios.get(url)
     .then(res => {
-      console.log(res.data['Stock Quotes']);
-      const stocks = Array.from(res.data['Stock Quotes']).map((stock) => [{symbol: stock.symbol, price: stock.price}]);
-      console.log(stocks);
+      let stocks = Array.from(res.data['Stock Quotes']).map((stock) => [{symbol: stock['1. symbol'], price: stock['2. price'], volume: stock['3. volume'], timestamp: stock['4. timestamp']}]);
+
+      this.setState((state, props) => {
+        return {
+          ...state,
+        stocks
+        }
+      })
     })
     .catch(error => console.log(error))
     )
-
   }
 
   render () {
+    let stocks = this.state.stocks;
     const value = this.state.value;
     return (
       <div className="App">
@@ -53,6 +60,7 @@ class App extends Component {
         <SearchBar value={ value }
                    onChange={ this.handleChange }
                    onClick={ this.handleClick }/>
+        <StockList stockItems={ stocks }/>
       </div>
     );
   }
---
+++ src/components/StockList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import StockListItem from './StockListItem';
+
+const StockList = (stockItems, props) => {
+console.log(props);
+
+  // const stockItem = props.stocks.map(({stock}) => {
+  //   console.log(stock);
+  //     return (
+  //       <StockListItem />
+  //     );
+  // });
+
+  return (
+    <ul className="StockList">
+
+    </ul>
+  )
+}
+
+export default StockList;
